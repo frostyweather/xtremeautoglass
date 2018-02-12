047d4450892385fa489117aec0aad310199c0c0a
Ian Frost <inf5000@gmail.com>
2018-02-12 02:01:38 -0500

    Added a link list on the homepage

--- page-homepage.php
+++ page-homepage.php
@@ -5,6 +5,150 @@
 		<?php include (TEMPLATEPATH . '/inc/hero.php');  ?>
 		<div class="l-container">
 			<?php the_content() ?>
+			<ul class="c-link-list ">
+
+				<li class="c-link-list__item">
+
+					<a href="#" class="c-link-list__link">
+						<div class="c-link-list__body">
+							<span class="c-link-list__link-text">Windshield Repairs</span>
+						</div>
+						<svg class="c-icon c-link-list__icon">
+							<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/wp-content/themes/xtremeautoglass/svg/icons.svg#caret-down"></use>
+						</svg>
+						<!--end c-icon-->
+					</a>
+					<!--end c-link-list__link-->
+
+				</li>
+				<!--end c-link-list__item-->
+
+				<li class="c-link-list__item">
+
+					<a href="#" class="c-link-list__link">
+						<div class="c-link-list__body">
+							<span class="c-link-list__link-text">Windshield Replacement</span>
+						</div>
+						<svg class="c-icon c-link-list__icon">
+							<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/wp-content/themes/xtremeautoglass/svg/icons.svg#caret-down"></use>
+						</svg>
+						<!--end c-icon-->
+					</a>
+					<!--end c-link-list__link-->
+
+				</li>
+				<!--end c-link-list__item-->
+
+				<li class="c-link-list__item">
+
+					<a href="#" class="c-link-list__link">
+						<div class="c-link-list__body">
+							<span class="c-link-list__link-text">Auto Glass Removal</span>
+						</div>
+						<svg class="c-icon c-link-list__icon">
+							<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/wp-content/themes/xtremeautoglass/svg/icons.svg#caret-down"></use>
+						</svg>
+					<!--end c-icon-->
+					</a>
+					<!--end c-link-list__link-->
+
+				</li>
+				<!--end c-link-list__item-->
+
+				<li class="c-link-list__item">
+
+					<a href="#" class="c-link-list__link">
+						<div class="c-link-list__body">
+							<span class="c-link-list__link-text">Door Window Replacement</span>
+						</div>
+						<svg class="c-icon c-link-list__icon">
+							<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/wp-content/themes/xtremeautoglass/svg/icons.svg#caret-down"></use>
+						</svg>
+						<!--end c-icon-->
+					</a>
+					<!--end c-link-list__link-->
+
+				</li>
+				<!--end c-link-list__item-->
+
+				<li class="c-link-list__item">
+
+					<a href="#" class="c-link-list__link">
+						<div class="c-link-list__body">
+							<span class="c-link-list__link-text">Rock Chip Repairs</span>
+						</div>
+						<svg class="c-icon c-link-list__icon">
+							<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/wp-content/themes/xtremeautoglass/svg/icons.svg#caret-down"></use>
+						</svg>
+						<!--end c-icon-->
+					</a>
+					<!--end c-link-list__link-->
+
+				</li>
+				<!--end c-link-list__item-->
+
+				<li class="c-link-list__item">
+
+					<a href="#" class="c-link-list__link">
+						<div class="c-link-list__body">
+							<span class="c-link-list__link-text">Door Motor Regulator Repair</span>
+						</div>
+						<svg class="c-icon c-link-list__icon">
+							<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/wp-content/themes/xtremeautoglass/svg/icons.svg#caret-down"></use>
+						</svg>
+						<!--end c-icon-->
+					</a>
+					<!--end c-link-list__link-->
+				</li>
+				<!--end c-link-list__item-->
+
+				<li class="c-link-list__item">
+
+					<a href="#" class="c-link-list__link">
+						<div class="c-link-list__body">
+							<span class="c-link-list__link-text">Classic Car Glass</span>
+						</div>
+						<svg class="c-icon c-link-list__icon">
+							<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/wp-content/themes/xtremeautoglass/svg/icons.svg#caret-down"></use>
+						</svg>
+						<!--end c-icon-->
+					</a>
+					<!--end c-link-list__link-->
+
+				</li>
+				<!--end c-link-list__item-->
+
+				<li class="c-link-list__item">
+
+					<a href="#" class="c-link-list__link">
+						<div class="c-link-list__body">
+							<span class="c-link-list__link-text">Vinyl Graphics</span>
+						</div>
+						<svg class="c-icon c-link-list__icon">
+							<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/wp-content/themes/xtremeautoglass/svg/icons.svg#caret-down"></use>
+						</svg>
+						<!--end c-icon-->
+					</a>
+					<!--end c-link-list__link-->
+
+				</li>
+				<!--end c-link-list__item-->
+
+				<li class="c-link-list__item">
+
+					<a href="#" class="c-link-list__link">
+						<div class="c-link-list__body">
+							<span class="c-link-list__link-text">Insurance Claims</span>
+						</div>
+						<svg class="c-icon c-link-list__icon">
+							<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/wp-content/themes/xtremeautoglass/svg/icons.svg#caret-down"></use>
+						</svg>
+						<!--end c-icon-->
+					</a>
+					<!--end c-link-list__link-->
+				</li>
+				<!--end c-link-list__item-->
+			</ul>
 		</div>
 	<?php endwhile; endif; ?>
 
